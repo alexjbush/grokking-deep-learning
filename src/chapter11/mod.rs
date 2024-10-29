20163b6109044ea2213d7d050c9d6adeef265239
Alex Bush <bushnoh@gmail.com>
2024-10-29 20:59:03 +1300

    More chapter 11

--- src/chapter11/mod.rs
+++ src/chapter11/mod.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 use std::time::{Duration, Instant};
 use crate::utils::{download_files, BASE_PATH, GROKKING_BASE_URL, read_lines};
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::iter::FromIterator;
 
 use crate::{Activity, Chapter};
@@ -62,6 +62,12 @@ fn softmax(
     return temp / sum;
 }
 
+fn sigmoid(
+    m: &ArrayBase<OwnedRepr<f64>, Dim<[usize; 1]>>,
+) -> ArrayBase<OwnedRepr<f64>, Dim<[usize; 1]>> {
+    m.map(|v| return 1.0 / (1.0 + (-1.0 * v).exp()))
+}
+
 fn chapter11a() -> Result<(), std::io::Error> {
 
     download_files(GROKKING_BASE_URL, BASE_PATH, FILES_TO_DOWNLOAD).unwrap();
@@ -72,6 +78,61 @@ fn chapter11a() -> Result<(), std::io::Error> {
     let tokens: Vec<HashSet<&str>> = raw_reviews.iter().map( |s| s.split_whitespace().collect::<HashSet<&str>>()).collect();
     
     let vocab: Vec<&str> = tokens.iter().flat_map(|sent| sent.iter().filter(|word| word.len() > 0)).collect::<HashSet<&&str>>().iter().map(|w| **w).collect();
+    
+    let word2index: HashMap<&str, usize> = vocab.iter().enumerate().map(|f| (*f.1, f.0)).collect();
+
+    let mut input_dataset: Vec<Vec<usize>> = vec![];
+    for sent in tokens {
+        let mut sent_indices: Vec<usize> = vec![];
+        for word in sent {
+            let idx = word2index.get(word);
+            if let Some(i) = idx {
+                sent_indices.push(*i);
+            }
+        }
+        input_dataset.push(sent_indices.into_iter().collect::<HashSet<usize>>().into_iter().collect())
+    }
+
+    let target_dataset: Vec<f64> = raw_labels.into_iter().map(|label| {
+        if label == "positive" {
+            return 1.0
+        } else {
+            return 0.0
+        }
+    }).collect();
+
+    let mut rng = ChaCha8Rng::seed_from_u64(1);
+
+    let alpha = 0.01;
+    let max_iterations = 2;
+    let hidden_size = 100;
+
+
+    let mut weights_0_1 = Array::random_using(
+        (vocab.len(), hidden_size),
+        Uniform::new(-0.1, 0.1),
+        &mut rng,
+    );
+    let mut weights_1_2 =
+        Array::random_using((hidden_size, 1), Uniform::new(-0.1, 0.1), &mut rng);
+
+    let mut correct = 0;
+    let mut total = 0;
+
+    for iteration in 0..max_iterations {
+
+        for i in 0..(input_dataset.len() - 1000) {
+
+            let x = &input_dataset[i];
+            let y = target_dataset[i];
+
+            let layer_1 = sigmoid(&weights_0_1.row(i).sum_axis(Axis(0)).insert_axis(Axis(0)));
+            let layer_2 = sigmoid(&layer_1.dot(&weights_1_2));
+
+        }
+
+    }
+
     todo!()
     
 }
