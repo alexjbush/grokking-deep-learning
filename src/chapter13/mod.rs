22e73a65cbef8e29a58c6a060f85f4b3ed62f0a1
Alex Bush <bushnoh@gmail.com>
2024-11-25 10:12:47 +1300

    Working API

--- src/chapter13/mod.rs
+++ src/chapter13/mod.rs
@@ -1,31 +1,53 @@
-use ndarray::ArrayD;
-use std::ops;
-
 use crate::{Activity, Chapter};
-
+use ndarray::array;
+use ndarray::ArrayD;
+use num_traits::Float;
+use std::{
+    collections::{HashMap, HashSet},
+    ops::{self},
+};
+use uuid::Uuid;
 pub const CHAPTER: Chapter = Chapter {
     activities: &ACTIVITIES,
     name: "Introducing automatic optimisation",
     id: "13",
 };
 
-const ACTIVITIES: [Activity; 0] = [];
+const CHAPTER13A: Activity = Activity {
+    task: chapter13a,
+    name: "TIntroduction to Autograd",
+    id: "13a",
+};
 
-trait Operation: 'static {}
+const ACTIVITIES: [Activity; 1] = [CHAPTER13A];
+
+trait Operation<T: Float> {
+    fn deriv(&self, grad: &Tensor<T>) -> Tensor<T>;
+}
 
 struct Add();
 
-impl Operation for Add {}
+impl<T: Float> Operation<T> for Add {
+    fn deriv(&self, grad: &Tensor<T>) -> Tensor<T> {
+        Tensor::new(grad.data.clone())
+    }
+}
+
+pub struct Gradients<'a, T: Float> {
+    grads: HashMap<Uuid, Tensor<'a, T>>,
+}
 
 pub struct Tensor<'a, T> {
+    id: Uuid,
     data: ArrayD<T>,
     creators: Vec<&'a Tensor<'a, T>>,
-    creation_op: Option<Box<dyn Operation>>,
+    creation_op: Option<Box<dyn Operation<T>>>,
 }
 
 impl<'a, T> Tensor<'a, T> {
     pub fn new(data: ArrayD<T>) -> Tensor<'a, T> {
         Tensor {
+            id: Uuid::new_v4(),
             data,
             creators: vec![],
             creation_op: None,
@@ -35,9 +57,10 @@ impl<'a, T> Tensor<'a, T> {
     fn new_with_creators(
         data: ArrayD<T>,
         creators: Vec<&'a Tensor<'a, T>>,
-        creation_op: Option<Box<dyn Operation>>,
+        creation_op: Option<Box<dyn Operation<T>>>,
     ) -> Tensor<'a, T> {
         Tensor {
+            id: Uuid::new_v4(),
             data,
             creators,
             creation_op,
@@ -49,9 +72,85 @@ impl<'a, T> Tensor<'a, T> {
     }
 }
 
+impl<'a, T> Tensor<'a, T>
+where
+    T: Float + Clone,
+{
+    pub fn get_gradient<'b>(&self, grads: &'b Gradients<'b, T>) -> &'b Tensor<'b, T> {
+        return &grads.grads.get(&self.id).unwrap();
+    }
+
+    pub fn backwards<'b>(&self, grad: &Tensor<'a, T>) -> Gradients<'a, T> {
+        let mut to_visit: Vec<(&Tensor<T>, HashSet<Uuid>)> = self
+            .creators
+            .iter()
+            .map(|v| (*v, HashSet::from([self.id])))
+            .collect();
+
+        let mut depends_on: HashMap<Uuid, Vec<&Tensor<T>>> = HashMap::new();
+
+        let mut grads = Gradients {
+            grads: HashMap::from([(self.id, Tensor::new(grad.data.clone()))]),
+        };
+
+        while let Some((t, h)) = to_visit.pop() {
+            t.creators.iter().for_each(|v| {
+                if h.contains(&t.id) {
+                    panic!(
+                        "Graph is cyclic, tensor with id {} found multiple times",
+                        t.id
+                    );
+                }
+                let mut new_set = h.clone();
+                new_set.insert(t.id);
+                to_visit.push((v, new_set));
+
+                if let Some(d) = depends_on.get_mut(&v.id) {
+                    d.push(t);
+                } else {
+                    depends_on.insert(v.id, vec![t]);
+                }
+            });
+        }
+
+        while !depends_on.is_empty() {
+            let maybe_t = depends_on.iter().find(|(_, contributors)| {
+                contributors.iter().all(|t| grads.grads.contains_key(&t.id))
+            });
+
+            if let Some((id, ts)) = maybe_t {
+                let mut t_n: Option<Tensor<T>> = None;
+
+                for t in ts {
+                    if let Some(acc) = t_n {
+                        let op = t.creation_op.as_ref().unwrap();
+                        let grad = grads.grads.get(&t.id).unwrap();
+                        let new_grad = op.deriv(grad);
+                        t_n = Some(Tensor::new(acc.data + new_grad.data));
+                    } else {
+                        t_n = Some(Tensor::new(t.data.clone()));
+                    }
+                }
+
+                if let Some(t) = t_n {
+                    grads.grads.insert(*id, t);
+                } else {
+                    panic!("Tensor with id {} has no upstream grads", id);
+                }
+            } else {
+                panic!(
+                    "Could not resolve all dependencies for remaining tensors: {:?}",
+                    depends_on.keys().collect::<Vec<&Uuid>>()
+                );
+            }
+        }
+        return grads;
+    }
+}
+
 impl<'a, T> ops::Add for &'a Tensor<'a, T>
 where
-    &'a ArrayD<T>: ops::Add<Output = ArrayD<T>>,
+    T: Float,
 {
     type Output = Tensor<'a, T>;
 
@@ -64,5 +163,19 @@ where
     }
 }
 
+fn chapter13a() -> Result<(), std::io::Error> {
+    let a = Tensor::new(array![1.0, 2.0, 3.0, 4.0, 5.0].into_dyn());
+    let b = Tensor::new(array![2.0, 2.0, 2.0, 2.0, 2.0].into_dyn());
+    let c = Tensor::new(array![5.0, 4.0, 3.0, 2.0, 1.0].into_dyn());
+    let d = Tensor::new(array![-1.0, -2.0, -3.0, -4.0, -5.0].into_dyn());
 
-todo backwards
+    let e = &a + &b;
+    let f = &c + &d;
+    let g = &e + &f;
+
+    let grads = g.backwards(&Tensor::new(array![1.0, 1.0, 1.0, 1.0, 1.0].into_dyn()));
+
+    println!("{:?}", &a.get_gradient(&grads).data);
+
+    Ok(())
+}
